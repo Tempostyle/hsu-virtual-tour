0476ebec3e43621a7cb454507b254fef728c495d
Jakob Fletcher <jakflet@aol.com>
2019-04-23 23:32:47 -0400

    Button management updated

--- interactives.js
+++ interactives.js
@@ -2,4 +2,12 @@ $(function(){
     $('#info1').click(function(){
       document.querySelector('#info1text').emit('popup');
     });
+    
+    $('#info2').click(function(){
+      document.querySelector('#info2text').emit('popup');
+    });
+  
+  $('#info3').click(function(){
+      document.querySelector('#info1text').emit('popup');
+    });
   });
